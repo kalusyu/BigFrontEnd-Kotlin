d4e4852850ea7dcddf7dc87dc662301be2d4b145
biaowen.yu <yubw@edde.com.cn>
2020-07-10 09:10:49 +0800

    [feat] android ndk 引用三方so文件配置

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,18 @@
 #include <jni.h>
 #include <string>
+#include <stdio.h>
+#include "mpp_err.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_kalusyu_bigfrontend_1kotlin_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+    int result = edde_mpp_main();
+//    char *hello = "Hello from C++";
+    const char *str = "abc";
+    char buff[30] = {0};
+
+    snprintf(buff, sizeof(buff), "%d%s", result, str);
+    return env->NewStringUTF(buff);
 }
