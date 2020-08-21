eba2acb25f9e0322787e28fa7fd524733349c9c6
kalusyu <kalusyu@aliyun.com>
2020-08-21 17:58:37 +0800

    [feat][buildSrc] buildSrc 添加

---
+++ buildSrc/src/main/java/com.kalusyu/Dept/Deps.kt
@@ -0,0 +1,16 @@
+package com.kalusyu.Dept
+
+/**
+ * desc:
+ *
+ * @author biaowen.yu
+ * @date 2020/8/21 17:23
+ *
+ **/
+object Versions {
+    val appcompat = "1.1.0"
+}
+
+object Deps {
+    val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+}
