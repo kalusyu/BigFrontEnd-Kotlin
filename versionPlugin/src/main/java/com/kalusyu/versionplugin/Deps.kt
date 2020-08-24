c2c21a580c1993d0336a6810a354735751100149
kalusyu <kalusyu@aliyun.com>
2020-08-24 11:09:34 +0800

    [feat][composing build] 复合构建

---
+++ versionPlugin/src/main/java/com/kalusyu/versionplugin/Deps.kt
@@ -0,0 +1,18 @@
+package com.kalusyu.versionplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * desc:
+ *
+ * @author biaowen.yu
+ * @date 2020/8/24 10:58
+ *
+ **/
+class Deps :Plugin<Project>{
+
+    override fun apply(target: Project) {
+        
+    }
+}
