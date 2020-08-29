f58d41a786bc09e1e9ad6af38bfa00d4693d595a
kalusyu <kalusyu@aliyun.com>
2020-08-29 23:02:29 +0800

    [netty] tcp/ip 拆包粘包例子，下一笔改造

---
+++ nettypractice/src/main/java/com/kalusyu/nettypractice/authority/chapter4/TimeClient.kt
@@ -0,0 +1,44 @@
+package com.kalusyu.nettypractice.authority.chapter4
+
+import io.netty.bootstrap.Bootstrap
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.ChannelOption
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioSocketChannel
+
+/**
+ * desc:
+ *
+ * @author biaowen.yu
+ * @date 2020/8/29 12:07
+ *
+ **/
+class TimeClient {
+
+    @Throws(Exception::class)
+    fun connect(port: Int, host: String) {
+        val group = NioEventLoopGroup()
+        try {
+            val b = Bootstrap()
+            b.group(group).channel(NioSocketChannel::class.java)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(object:ChannelInitializer<SocketChannel>(){
+                    override fun initChannel(ch: SocketChannel?) {
+                        ch?.pipeline()?.addLast(TimeClientHandler())
+                    }
+                })
+
+            val future = b.connect(host,port).sync()
+
+            future.channel().closeFuture().sync()
+        } finally {
+            group.shutdownGracefully()
+        }
+    }
+}
+
+fun main() {
+    TimeClient().connect(PORT, "127.0.0.1")
+}
+
---
+++ nettypractice/src/main/java/com/kalusyu/nettypractice/authority/chapter4/TimeServerHandler.kt
@@ -0,0 +1,38 @@
+package com.kalusyu.nettypractice.authority.chapter4
+
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.Unpooled
+import io.netty.channel.ChannelHandlerAdapter
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.SimpleChannelInboundHandler
+import java.nio.charset.Charset
+import java.util.*
+
+/**
+ * desc:
+ *
+ * @author biaowen.yu
+ * @date 2020/8/29 11:42
+ *
+ **/
+class TimeServerHandler:SimpleChannelInboundHandler<Any>() {
+    var counter:Int = 0
+
+    override fun channelRead0(ctx: ChannelHandlerContext?, msg: Any?) {
+        val buf = msg as ByteBuf
+        val req = ByteArray(buf.readableBytes())
+        buf.readBytes(req)
+        val body = String(req, Charset.defaultCharset()).substring(0, req.size - System.getProperty("line.separator")!!.length)
+        println("body=${body} ; the counter is : ${++counter}")
+        var currentTime = if (CMD.equals(body, true)) Date(System.currentTimeMillis()).toString() else "BAD ORDER"
+
+        currentTime += System.getProperty("line.separator")
+        val resp = Unpooled.copiedBuffer(currentTime.toByteArray(Charset.defaultCharset()))
+        ctx?.write(resp)
+
+    }
+
+    override fun channelReadComplete(ctx: ChannelHandlerContext?) {
+        ctx?.flush()
+    }
+}
