b790cafe2fb92fa8cfea385bad34a7f6b8713d3c
kalusyu <kalusyu@aliyun.com>
2020-11-26 17:24:11 +0800

    feat(jetpack): jetpack lifecycle模块添加

---
+++ aicameradatatest/src/main/java/com/kalus/aicameradatatest/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kalus.aicameradatatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * desc: AI摄像头数据压力测试验证
+ *
+ * @author biaowen.yu
+ * @date 2020/11/3 16:18
+ *
+ **/
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
