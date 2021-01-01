5bc5aaaedebebed10e4dbde35492dc2e4af1ec88
kalusyu <kalusyu@aliyun.com>
2021-01-01 10:46:28 +0800

    [Activity] activity横竖屏切换生命周期变化和配置configChanges的场景

--- IV/src/main/java/com/kaluus/iv/MainActivity.kt
+++ IV/src/main/java/com/kaluus/iv/MainActivity.kt
@@ -2,8 +2,10 @@ package com.kaluus.iv
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
@@ -13,10 +15,14 @@ import com.kaluus.iv.fragment.viewmodel.ViewModelActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e(TAG, "onCreate")
 
         val list = mutableListOf("权限管理", "Fragment使用大全", "DemoFragment","ViewModelFragment")
         val adapter =
@@ -51,6 +57,43 @@ class MainActivity : AppCompatActivity() {
 
     fun Context.showToast(text: String) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e(TAG, "onRestart")
+    }
+
+    // 非生命周期方法
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        Log.e(TAG, "onConfigurationChanged")
     }
 }
 
